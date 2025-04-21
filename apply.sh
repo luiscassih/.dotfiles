9035d895bbb1785c4c11010f1bc0bfc7364d96b6
Luis C <luiscassih@gmail.com>
2025-04-21 20:23:49 -0300

    added possible desktop entries fix

--- apply.sh
+++ apply.sh
@@ -1,1 +1,18 @@
-home-manager switch --flake .#lain
+APPLY=true
+CLEAN=false
+for arg in "$@"; do
+  case "$arg" in
+    --clean)
+      CLEAN=true
+      APPLY=false
+      ;;
+  esac
+done
+
+if [ "$CLEAN" = true ]; then
+  nix-collect-garbage -d
+fi
+
+if [ "$APPLY" = true ]; then
+  home-manager switch --flake .#lain
+fi
--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 let
   configPath = "/home/l/.dotfiles";
   symlink = config.lib.file.mkOutOfStoreSymlink;
@@ -64,7 +64,7 @@ in {
     #   org.gradle.daemon.idletimeout=3600000
     # '';
     # ".config/nvim".source = symlink configPath + /dotfiles/.config/nvim;
-    ".zshrc".source = symlink configPath + /dotfiles/.config/.zshrc;
+    ".zshrc".source = symlink configPath + /dotfiles/.zshrc;
   };
 
   # Home Manager can also manage your environment variables through
@@ -100,5 +100,40 @@ in {
       };
     };
     starship.enable = true;
+  };
+  programs.bash.profileExtra = lib.mkAfter ''
+    rm -rf ${config.home.homeDirectory}/.local/share/applications/home-manager
+    rm -rf ${config.home.homeDirectory}/.icons/nix-icons
+    ls ${config.home.homeDirectory}/.nix-profile/share/applications/*.desktop > ${config.home.homeDirectory}/.cache/current_desktop_files.txt
+  '';
+  home.activation = {
+    linkDesktopApplications = {
+      after = ["writeBoundary" "createXdgUserDirectories"];
+      before = [];
+      data = ''
+        rm -rf ${config.home.homeDirectory}/.local/share/applications/home-manager
+        rm -rf ${config.home.homeDirectory}/.icons/nix-icons
+        mkdir -p ${config.home.homeDirectory}/.local/share/applications/home-manager
+        mkdir -p ${config.home.homeDirectory}/.icons
+        ln -sf ${config.home.homeDirectory}/.nix-profile/share/icons ${config.home.homeDirectory}/.icons/nix-icons
+
+        # Check if the cached desktop files list exists
+        if [ -f ${config.home.homeDirectory}/.cache/current_desktop_files.txt ]; then
+          current_files=$(cat ${config.home.homeDirectory}/.cache/current_desktop_files.txt)
+        else
+          current_files=""
+        fi
+
+        # Symlink new desktop entries
+        for desktop_file in ${config.home.homeDirectory}/.nix-profile/share/applications/*.desktop; do
+          if ! echo "$current_files" | grep -q "$(basename $desktop_file)"; then
+            ln -sf "$desktop_file" ${config.home.homeDirectory}/.local/share/applications/home-manager/$(basename $desktop_file)
+          fi
+        done
+
+        # Update desktop database
+        ${pkgs.desktop-file-utils}/bin/update-desktop-database ${config.home.homeDirectory}/.local/share/applications
+      '';
+    };
   };
 }
